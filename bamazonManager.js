aa8fce298b63413f88fc5b78b0fcc81dd9955ea3
claire-labry <clairestjlabry@gmail.com>
2020-04-22 15:49:17 -0500

    completed bamazonManager functionalities

--- bamazonManager.js
+++ bamazonManager.js
@@ -62,7 +62,7 @@ function productSale(){
     });
 
     for(var i = 0; i<res.length; i++){
-        table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quanity],
+        table.push([res[i].item_id, res[i].product_name, res[i].department_name, '$' + res[i].price, res[i].stock_quanity],
       );
     }
     console.log(table.toString());
@@ -88,14 +88,22 @@ function lowInventory(){
 }
 
 function addInventory(){
+
+  connection.query('SELECT * FROM products', function (err, res){
+    if(err) throw err;
+    var inventory = []
+
+    for(var i = 0; i < res.length; i++){
+      inventory.push(res[i].product_name);
+    }
   inquirer
   .prompt([
     {
       name:'id',
       type: 'input',
-       message: 'Please give me the id of the product that you would like to add inventory to.',
+       message: 'Product ID of the item you would like to add inventory to: ',
        validate: function(value){
-           if(isNaN(value)){
+        if(isNaN(value) === false && parseInt(value) <= res.length && parseInt(value) > 0){
                return true;
            } else{
 
@@ -107,12 +115,110 @@ function addInventory(){
        type: 'input',
        message: 'How much inventory would you like to add?',
        validate: function(value){
-           if(isNaN(value)){
+        if(isNaN(value) === false){
                return true;
            } else {
                return false;
            }
        }
    }
-]);
-} 
+])
+.then(function(answer){
+
+  connection.query ('SELECT * FROM products', function(err,res){
+      if(err)throw err;
+
+      increaseInventory = '';
+
+      for(var i = 0; i < res.length; i++) {
+        if(res[i].item_id === parseInt(answer.id)){
+          increaseInventory = res[i];
+        }
+      }
+     connection.query('UPDATE products SET ? WHERE ?', [
+       {
+        stock_quanity: increaseInventory.stock_quanity += parseInt(answer.quanity)
+       },
+       {
+       item_id: increaseInventory.item_id
+       }
+     ],
+     function(err){
+       if(err) throw err; 
+
+       console.log('Successfully added more inventory to: ' + answer.quanity + ' ' + increaseInventory.product_name);
+       productSale();
+        })
+      });
+    });
+  })
+};
+
+function addProduct(){
+
+  connection.query('SELECT * FROM products WHERE department_name', function (err, res){
+    if(err) throw err;
+
+    for(var i = 0; i < res.length; i++){
+      department.push(res[i].department_name);
+    }
+  })
+
+  inquirer
+  .prompt([
+    {
+      name: 'product',
+      type: 'input',
+      message: 'Please input the product name that you would like to add:',
+      validate: function(value){
+        if(value){
+          return true;
+        } else{
+          return false;
+        }
+      }
+    }, 
+      {
+        name: 'department',
+        type: 'list',
+        message: 'what dapartment would you like to put this product in?',
+        choices: ['Books','Bedrooom','Kitchen', 'Garden', 'Home Improvement', 'Outdoor', 'Pets', 'Sports']
+      },
+      {
+        name: 'price',
+        type: 'input',
+        message: 'What is the price of the product that you would like to add?',
+        validate: function(value){
+          if(isNaN(value)=== false){
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },
+      {
+        name: 'quanity',
+        type: 'input',
+        message: 'Please provide the quanity of the product that you would like to add:',
+        validate: function(value){
+          if(isNaN(value)=== false){
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+  ])
+  .then(function(answer){
+    connection.query('INSERT INTO products SET ?',{
+      product_name: answer.product,
+      department_name: answer.department,
+      price: answer.price,
+      stock_quanity: answer.quanity
+    },function(err, res){
+      if(err) throw err;
+      console.log('You have successfully added a new product to the bamazon inventory!')
+    })
+    productSale();
+  })
+}
